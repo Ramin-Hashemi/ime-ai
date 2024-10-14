4685ae4e2a8761b7c15feaad0e66c4139a5f4c8c
Ramin Hashemi <ramin.hashemi@usa.com>
2024-10-15 01:27:38 +0330

    remove unused files

--- main_gunicorn.py
+++
@@ -1,9 +0,0 @@
-#########################
-# File main_gunicorn.py #
-#########################
-
-from main import app
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="89.32.250.198", port=8000)
