1b8f0d7f479358c289b54c8685c86e6342c97aa0
Ramin Hashemi <ramin.hashemi@usa.com>
2024-10-17 07:19:34 +0330

    minor updates

--- run_script.sh
+++ run_script.sh
@@ -8,15 +8,3 @@ python server_config.py "$1"
 
 # Keep the shell open
 exec "$SHELL"
-
-# # Get the function name from the first argument
-# function_name=$1
-
-# # Check if the function name is provided
-# if [ -z "$function_name" ]; then
-#     echo "Please provide a function name to run."
-#     exit 1
-# fi
-
-# # Call the function
-# $function_name
